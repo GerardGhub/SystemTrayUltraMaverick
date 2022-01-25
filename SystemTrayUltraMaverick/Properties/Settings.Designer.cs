d40e7c3189e121e058a8c799436e8b08654aee10
gerard singian <gerard.singian@outlook.com>
2022-01-25 11:29:11 +0800

    Properties Settings // for Database Connections

--- SystemTrayUltraMaverick/Properties/Settings.Designer.cs
+++ SystemTrayUltraMaverick/Properties/Settings.Designer.cs
@@ -8,21 +8,81 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-namespace SystemTrayUltraMaverick.Properties
-{
+namespace SystemTrayUltraMaverick.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\MaverickReports")]
+        public string rpt_path {
+            get {
+                return ((string)(this["rpt_path"]));
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\MaverickReports")]
+        public string fdg {
+            get {
+                return ((string)(this["fdg"]));
+            }
+            set {
+                this["fdg"] = value;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=MIS-GSINGIAN-L;Initial Catalog=UltraMaverickDB;Integrated Security=SS" +
+            "PI;")]
+        public string hr_bakConnectionString {
+            get {
+                return ((string)(this["hr_bakConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=MIS-GSINGIAN-L;Initial Catalog=UltraMaverickDB;Integrated Security=SS" +
+            "PI;")]
+        public string hr_application_conn2 {
+            get {
+                return ((string)(this["hr_application_conn2"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=MIS-GSINGIAN-L;Initial Catalog=UltraMaverickDB;Integrated Security=SS" +
+            "PI;")]
+        public string hr_application_conn {
+            get {
+                return ((string)(this["hr_application_conn"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=MIS-GSINGIAN-L;Initial Catalog=UltraMaverickDB;Integrated Security=Tr" +
+            "ue")]
+        public string UltraMaverickDBConnectionString {
+            get {
+                return ((string)(this["UltraMaverickDBConnectionString"]));
             }
         }
     }
